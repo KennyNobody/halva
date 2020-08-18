7ab7e56d8d0ef7f249a8d08acd392ddb770de541
kenny_nobody <nobodykenny@gmail.com>
2020-08-18 03:56:42 +0200

    'final'

--- src/blocks/modules/ui/ui.js
+++ src/blocks/modules/ui/ui.js
@@ -22,3 +22,26 @@
 		}
 	}
 })();
+
+(function toggleTextareaTitle(){
+	const inputs = document.querySelectorAll('.textarea__field');
+
+	if (inputs) {
+		for(let i = 0; i < inputs.length; i++) {
+			inputs[i].addEventListener('focus', addActiveClass);
+			inputs[i].addEventListener('focusout', removeActiveClass);
+		}
+	}
+
+	function addActiveClass() {
+		if (window.innerWidth < 1200) {
+			this.parentNode.classList.add('textarea--focus');
+		}	
+	}
+
+	function removeActiveClass() {
+		if (window.innerWidth < 1200 && this.value == false) {
+			this.parentNode.classList.remove('textarea--focus');
+		}
+	}
+})();
